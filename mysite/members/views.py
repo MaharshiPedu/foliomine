77b88af424638491c9e221eccd29bf8712486a98
MaharshiPedu <pandyamh@rknec.edu>
2022-12-27 11:58:53 +0530

    Added logout view \n 1. Added logout view in views.py \n 2. Added a new url in urls.py \n 3. Modified index.html to add logout link that disappears after a user logs out

--- mysite/members/views.py
+++ mysite/members/views.py
@@ -18,3 +18,9 @@ def login_user(request):
 
     else:
         return render(request, 'registration/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('index')
