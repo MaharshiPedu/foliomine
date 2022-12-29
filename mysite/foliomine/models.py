09de9267da78c37db7f37fca28edf641b1b2237e
MaharshiPedu <pandyamh@rknec.edu>
2022-12-29 11:23:34 +0530

    Removed User model from models.py of Foliomine

--- mysite/foliomine/models.py
+++ mysite/foliomine/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class User(models.Model):
-
-#     first_name      = models.CharField(max_length=25, null=False)
-#     last_name       = models.CharField(max_length=25, null=False)
-#     email           = models.EmailField(max_length=50, null=False)
-#     hashed_password = models.CharField(max_length=100, null=False)
-
-#     class Meta:
-#         db_table = "User"
-
-#     def __str__(self):
-#         return self.first_name+" "+self.last_name+" user"
-
 
 class Profile(models.Model):
 
