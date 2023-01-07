c36311db67c419e4f4484bd20ceb8948fc904da7
MaharshiPedu <pandyamh@rknec.edu>
2023-01-07 18:22:51 +0530

    new profile creation without profile picture completed

--- mysite/foliomine/forms.py
+++ mysite/foliomine/forms.py
@@ -7,4 +7,4 @@ class CreateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'about', 'github_link', 'twitter_link', 'linkedin_link')
-        labels = {'first_name':'First Name:', 'last_name':'Last Name:', 'about':'About:', 'github_link':'GitHub Link:', 'twitter_link':'Twitter Link:', 'linkedin_link':'LinkedIn Link:'}
+        labels = {'first_name':'First Name', 'last_name':'Last Name', 'about':'About', 'github_link':'GitHub Link', 'twitter_link':'Twitter Link', 'linkedin_link':'LinkedIn Link'}
